9d315d2627e3e74231cbf9cb53783a0c0bebe18e
Brian Kirchoff <briankircho@gmail.com>
2008-10-15 07:57:39 +0000

    AJAX save button/plugin

---
+++ nicSave/nicSave.js
@@ -0,0 +1,30 @@
+/**
+ * nicSave
+ * @description: A button for nicEdit ajax content saving
+ * @requires: nicCore, nicButton
+ * @author: Brian Kirchoff
+ * @version: 0.9.0
+ */
+
+/* START CONFIG */
+var nicSaveOptions = {
+	buttons : {
+		'save' : {name : __('Save this content'), type : 'nicEditorSaveButton'}
+	}/* NICEDIT_REMOVE_START */,iconFiles : {'save' : 'src/nicSave/icons/save.gif'}/* NICEDIT_REMOVE_END */
+};
+/* END CONFIG */
+
+var nicEditorSaveButton = nicEditorButton.extend({
+	init : function() {
+		if(!this.ne.options.onSave) {
+			this.margin.setStyle({'display' : 'none'});
+		}
+	},
+	mouseClick : function() {
+		var onSave = this.ne.options.onSave;
+		var selectedInstance = this.ne.selectedInstance;
+		onSave(selectedInstance.getContent(), selectedInstance.elm.id, selectedInstance);
+	}
+});
+
+nicEditors.registerPlugin(nicPlugin,nicSaveOptions);
