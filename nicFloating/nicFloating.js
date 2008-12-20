03ca573130d6a2beba820b41775d47aeb801637c
Brian Kirchoff <briankircho@gmail.com>
2008-12-20 03:18:39 +0000

    nicFloating, a plugin to create floating editor panels

---
+++ nicFloating/nicFloating.js
@@ -0,0 +1,30 @@
+/**
+ * NicEdit Floating Panel Support
+ * @description: Create a new floating panel, that automatically positions itself above the last selected editor instance
+ * @author: Brian Kirchoff
+ * @version: 0.9.0
+ * @requires: nicCore
+ */
+
+nicEditor = nicEditor.extend({
+        floatingPanel : function() {
+                this.floating = new bkElement('DIV').setStyle({position: 'absolute', top : '-1000px'}).appendTo(document.body);
+                this.addEvent('focus', this.reposition.closure(this)).addEvent('blur', this.hide.closure(this));
+                this.setPanel(this.floating);
+        },
+        
+        reposition : function() {
+                var e = this.selectedInstance.e;
+                this.floating.setStyle({ width : (parseInt(e.getStyle('width')) || e.clientWidth)+'px' });
+                var top = e.offsetTop-this.floating.offsetHeight;
+                if(top < 0) {
+                        top = e.offsetTop+e.offsetHeight;
+                }
+                
+                this.floating.setStyle({ top : top+'px', left : e.offsetLeft+'px', display : 'block' });
+        },
+        
+        hide : function() {
+                this.floating.setStyle({ top : '-1000px'});
+        }
+});
